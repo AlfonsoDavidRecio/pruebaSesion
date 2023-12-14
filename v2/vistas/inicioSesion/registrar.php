ae8ebe83997c1259df0c70c1b1ec32a0a7614312
Alfonso David Recio Calderon <alfonsodavidreciocalderon.guadalupe@alumnado.fundacionloyola.es>
2023-12-14 16:16:00 +0100

    Hecho estilo de la pagina de registro

--- v2/vistas/inicioSesion/registrar.php
+++ v2/vistas/inicioSesion/registrar.php
@@ -1,13 +1,116 @@
-<?php
+<?php session_start(); //Inicio de sesion ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+            text-align: center; /* Añadido para centrar el contenido */
+        }
 
-//Inicio Sesion
-session_start();
+        main {
+            margin: 50px auto;
+            border-radius: 5px;
+        }
 
-//Existe datos en $_SESSION
-if(isset($_SESSION['nombre'])) {
-    echo $_SESSION['nombre'].' Bienvenido/a SUPER ADMIN, registra mas admins';
-} else {
-    //Si no hay datos en $_SESSION muestra mensaje
-    echo "No se encontraron datos de sesión.";
-}
-?>
+        section {
+            background-color: #fff;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            padding: 20px;
+            width: 300px;
+            margin: 0 auto; /* Añadido para centrar el section horizontalmente */
+        }
+
+        h1 {
+            text-align: center;
+            color: #333;
+        }
+
+        form {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        label {
+            color: #555;
+            font-weight: bold;
+        }
+
+        input {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 10px;
+            margin-top: 5px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            box-sizing: border-box;
+        }
+
+        input[type="submit"] {
+            background-color: #4caf50;
+            color: #fff;
+            cursor: pointer;
+        }
+
+        p {
+            text-align: left;
+            width: 100%;
+            margin-bottom: 15px;
+        }
+    </style>
+</head>
+<body>
+    <main>
+        <section>
+            <div>
+                <p>
+                    <?php
+                        //Existe datos en $_SESSION
+                        if(isset($_SESSION['nombre'])) {
+                            echo '<p>'.$_SESSION['nombre'].' En esta página podras registrar a los admins para los minijuegos</p>';
+                        } else {
+                            //Si no hay datos en $_SESSION muestra mensaje
+                            echo "No se encontraron datos de sesión.";
+                        }
+                    ?>
+                </p>
+            </div>
+        </section>
+        <section>
+            <h1>Registrar Admin Minijuegos</h1>
+            <hr>
+            <div>
+                <form action="../index.php?control=sesion_con&metodo=register" method="POST">
+                    <p>
+                        <label for="Introducir Nombre">Nombre</label>
+                        <br/>
+                        <input type="text" name="nombre" placeholder="Introducir Nombre">
+                    </p>
+                    <p>
+                        <label for="Correo Electronico">Correo</label>
+                        <br/>
+                        <input type="text" name="correo" placeholder="Correo Electronico">
+                    </p>
+                    <p>
+                        <label for="Introducir Contraseña">Contraseña</label>
+                        <br/>
+                        <input type="text" name="pw" placeholder="Introducir Contraseña">
+                    </p>
+                    <p>
+                        <label for="Perfil de Administrador">Perfil</label>
+                        <br/>
+                        <input type="text" name="perfil" placeholder="Perfil de Administrador">
+                    </p>
+                    <input type="submit">
+                </form>
+            </div>
+            <?php echo "<p style='color: red; text-align: center;'>".$mensaje."</p>" ?>
+        </section>
+    </main>
+</body>
